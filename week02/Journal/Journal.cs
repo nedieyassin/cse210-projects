3bdc7030e5f4bcde72e9b07fac85ab7cfc2587dc
Nedie Yassin <yassinnedie9121@gmail.com>
2025-01-18 22:34:28 +0200

    W2-2

---
+++ week02/Journal/Journal.cs
@@ -0,0 +1,66 @@
+public class Journal
+{
+    public List<Entry> _entries = new List<Entry>();
+
+    public void AddEntry(Entry entry)
+    {
+          _entries.Add(entry);
+    }
+
+    public void DisplayAll()
+    {
+        foreach (var entry in _entries)
+        {
+            entry.Display();
+            Console.WriteLine();
+        }
+    }
+
+    public void SaveToFile(string filename)
+    {
+        using (StreamWriter outputFile = new StreamWriter($"{filename}.csv"))
+        {
+
+            outputFile.WriteLine("Date,PromptText,EntryText");
+            foreach (var entry in _entries)
+            {
+                outputFile.WriteLine($"{entry._date},{entry._promptText},{entry._entryText}");
+            }
+
+        }
+    }
+
+
+    public void LoadFromFile(string filename)
+    {
+        string[] lines = System.IO.File.ReadAllLines($"{filename}.csv");
+
+        List<Entry> entries = new List<Entry>();
+
+        for (int i = 0; i < lines.Length; i++)
+        {
+
+
+            // Skip headers
+            if (i == 0)
+            {
+                continue;
+            }
+            string[] parts = lines[i].Split(",");
+
+            string date = parts[0];
+            string promptText = parts[1];
+            string entryText = parts[2];
+
+            Entry entry = new Entry();
+            entry._date = date;
+            entry._promptText = promptText;
+            entry._entryText = entryText;
+
+            entries.Add(entry);
+        }
+
+        _entries = entries;
+    }
+
+}
--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -4,6 +4,65 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+
+        // I exceeded requirements by adding ability to save .csv file which can be opened with Excel.
+        Journal journal = new Journal();
+
+        while (true)
+        {
+            Console.WriteLine("Please select one if the following choices:");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Quit");
+
+
+            Console.Write("What would you like to do?: ");
+            int input = int.Parse(Console.ReadLine());
+
+
+
+            if (input == 1)
+            {
+
+                PromptGenerator promptGenerator = new PromptGenerator();
+                Entry entry = new Entry();
+
+                string promptText = promptGenerator.GetRandomPrompt();
+
+                Console.WriteLine(promptText);
+                string entryText = Console.ReadLine();
+
+                entry._date = DateTime.Now.ToShortDateString();
+                entry._promptText = promptText;
+                entry._entryText = entryText;
+
+                journal.AddEntry(entry);
+
+            }
+            else if (input == 2)
+            {
+                journal.DisplayAll();
+            }
+            else if (input == 3)
+            {
+                Console.WriteLine("What is the filename? (ignore the file extension)");
+                string filename = Console.ReadLine();
+                journal.LoadFromFile(filename);
+            }
+            else if (input == 4)
+            {
+                Console.WriteLine("What is the filename? (ignore the file extension)");
+                string filename = Console.ReadLine();
+                journal.SaveToFile(filename);
+            }
+            else if (input == 5)
+            {
+                Console.WriteLine("Goodbye, have a blessed day.");
+                break;
+            }
+
+        }
     }
 }
