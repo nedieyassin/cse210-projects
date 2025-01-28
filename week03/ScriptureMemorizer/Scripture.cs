8c935d15f00361ce83b72adfcdb5cfc1f870aff7
Nedie Yassin <yassinnedie9121@gmail.com>
2025-01-28 22:39:44 +0200

    W3-2

---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,29 @@
+public class Scripture
+{
+    private Reference _reference;
+    private Word _words;
+
+    public Scripture(Reference reference, string text)
+    {
+
+    }
+
+
+    public void HideRandomWords(int numberToHide)
+    {
+
+    }
+
+    public string GetDisplayText()
+    {
+
+        return "";
+    }
+
+
+    public bool IsCompletelyHidden()
+    {
+
+        return false;
+    }
+}
