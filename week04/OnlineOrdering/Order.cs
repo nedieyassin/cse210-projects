365fcbcb08532dcde69f16c00115be2180fe9a3a
Nedie Yassin <yassinnedie9121@gmail.com>
2025-02-05 14:10:56 +0200

    W4-2

---
+++ week04/OnlineOrdering/Order.cs
@@ -0,0 +1,42 @@
+public class Order
+{
+    private List<Product> _products;
+    private Customer _customer;
+
+    public Order(Customer customer)
+    {
+        this._customer = customer;
+        this._products = new List<Product>();
+    }
+
+    public void AddProduct(Product product)
+    {
+        _products.Add(product);
+    }
+
+    public double GetTotalPrice()
+    {
+        double total = 0;
+        foreach (var product in _products)
+        {
+            total += product.GetTotalCost();
+        }
+        total += _customer.IsInUSA() ? 5 : 35;
+        return total;
+    }
+
+    public string GetPackingLabel()
+    {
+        string label = "Packing List:\n";
+        foreach (var product in _products)
+        {
+            label += product.GetPackingLabel() + "\n";
+        }
+        return label;
+    }
+
+    public string GetShippingLabel()
+    {
+        return "Shipping To:\n" + _customer.GetShippingLabel();
+    }
+}
---
+++ week04/OnlineOrdering/Product.cs
@@ -0,0 +1,25 @@
+public class Product
+{
+    private string _name;
+    private string _productId;
+    private double _price;
+    private int _quantity;
+
+    public Product(string name, string productId, double price, int quantity)
+    {
+        this._name = name;
+        this._productId = productId;
+        this._price = price;
+        this._quantity = quantity;
+    }
+
+    public double GetTotalCost()
+    {
+        return _price * _quantity;
+    }
+
+    public string GetPackingLabel()
+    {
+        return $"{_name} (ID: {_productId})";
+    }
+}
